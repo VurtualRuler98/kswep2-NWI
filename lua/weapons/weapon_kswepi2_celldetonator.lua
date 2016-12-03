3c5e2350c118423c46429c832f9b589e2bd8f3e5
vurtual <VurtualRuler98@gmail.com>
2016-12-02 21:09:44 -0600

    M57 and Cellphone detonators, first version

---
+++ lua/weapons/weapon_kswepi2_celldetonator.lua
@@ -0,0 +1,21 @@
+if (SERVER) then
+	AddCSLuaFile()
+end
+if (CLIENT) then
+	SWEP.PrintName = "Ins2 Cellphone Detonator"
+	SWEP.Author = "vurtual"
+	SWEP.Slot = 5
+	SWEP.SlotPos = 0
+end
+SWEP.Base="weapon_ksweps_detonator"
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.Category="Vurtual's Insurgency 2 K-Weapons"
+SWEP.ViewModel = "models/weapons/v_ied_ins.mdl"
+SWEP.WorldModel = "models/weapons/w_slam.mdl"
+SWEP.UseInsHands=true
+function SWEP:DiscoverModelAnims()
+	self:SetAnim("Idle",self:DiscoverAnim("ACT_VM_IDLE_DET"))
+	self:SetAnim("Draw",self:DiscoverAnim("ACT_VM_DRAW_DET"))
+	self:SetAnim("Detonate",self:DiscoverAnim("ACT_VM_PRIMARYATTACK"))
+end
