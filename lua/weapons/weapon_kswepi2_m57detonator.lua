4491ff10ccf3726677d01687eba30b7dab4525e8
vurtual <VurtualRuler98@gmail.com>
2016-12-03 18:24:06 -0600

    Detonators now have delays

--- lua/weapons/weapon_kswepi2_m57detonator.lua
+++ lua/weapons/weapon_kswepi2_m57detonator.lua
@@ -14,6 +14,7 @@ SWEP.Category="Vurtual's Insurgency 2 K-Weapons"
 SWEP.ViewModel = "models/weapons/v_c4_sec.mdl"
 SWEP.WorldModel = "models/weapons/w_slam.mdl"
 SWEP.UseInsHands=true
+SWEP.DetonatorDelay=0.5
 function SWEP:DiscoverModelAnims()
 	self:SetAnim("Idle",self:DiscoverAnim("ACT_VM_IDLE_DET"))
 	self:SetAnim("Draw",self:DiscoverAnim("ACT_VM_DRAW_DET"))
